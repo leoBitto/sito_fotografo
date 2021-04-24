29d330f5dedbe30b0748209944195ba34382c96d
leoBitto <leonardobitto@gmail.com>
2021-04-24 11:46:11 +0200

    fatto masonry, aggiustato admin change

--- main/admin.py
+++ main/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Photo
 
 class PhotoAdmin(admin.ModelAdmin):
-    list_display = ('url_immagine', 'ruolo', 'posizione')
+    list_display = ('id', 'url_immagine', 'ruolo', 'posizione')
 
 admin.site.register(Photo, PhotoAdmin)
--- main/views.py
+++ main/views.py
@@ -16,7 +16,25 @@ def menu(request):
 
 #pagina portfolio
 def portfolio(request):
-    context={}
+    # richiama tutte le foto portfolio
+    photo_portfolio = Photo.objects.filter(ruolo='foto_portfolio')
+    print(photo_portfolio)
+
+    # dividile in due queryset, pari e dispari
+    foto_pari =[]
+    foto_dispari=[]
+    for photo in photo_portfolio:
+        if photo.posizione % 2 == 0:  # se la posizione è pari
+            foto_pari.append(photo)   # aggiungi a foto pari
+        else:
+            foto_dispari.append(photo)# aggiungi a foto dispari
+
+    # inserisci i dizionari nel context
+    context={
+        'foto_pari': foto_pari,
+        'foto_dispari': foto_dispari,
+        'media_url': settings.MEDIA_URL,
+    }
     return render(request, 'portfolio.html', context)
 
 
