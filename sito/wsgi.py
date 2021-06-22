db3565b30609fc233a6fef63be62dc4a95a098d2
leoBitto <leonardobitto@gmail.com>
2021-06-22 11:03:42 +0000

    fixed wsgi.py

--- sito/wsgi.py
+++ sito/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, BASE_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'sito.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sito.settings')
 
 application = get_wsgi_application()
