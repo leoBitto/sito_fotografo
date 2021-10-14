542601d353cf7af85cb902c7e829e6c766fd7e94
leoBitto <leonardobitto@gmail.com>
2021-10-14 14:20:37 +0100

    nginx conf, sito/dockerfile, sito/settings.py

--- sito/sito/settings.py
+++ sito/sito/settings.py
@@ -20,12 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = '2pm(7eranx=+1!zrk%bx%8&znxvh0x4%b&=t5_*80nbpkjm&=!'
+#os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = True
+#int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+
+#os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
