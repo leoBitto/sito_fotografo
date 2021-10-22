040786012129a010731461b88fb5341f3312dacc
leoBitto <leonardobitto@gmail.com>
2021-10-22 22:15:50 +0100

    modified env settings

--- sito/sito/settings.py
+++ sito/sito/settings.py
@@ -20,16 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2pm(7eranx=+1!zrk%bx%8&znxvh0x4%b&=t5_*80nbpkjm&=!'
-#os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-#int(os.environ.get("DEBUG", default=0))
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ['*', '192.168.0.172', '127.0.0.1', '0.0.0.0']
-
-#os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")  'localhost', '127.0.0.1', '[::1]'
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")  
 
 
 # Application definition
