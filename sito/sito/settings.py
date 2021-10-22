8c037ca1cf977e58563994d8aec2a8aefc48fd93
leoBitto <leonardobitto@gmail.com>
2021-10-22 22:44:50 +0100

    taken out reference to setting external file

--- sito/sito/settings.py
+++ sito/sito/settings.py
@@ -20,12 +20,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = '2pm(7eranx=+1!zrk%bx%8&znxvh0x4%b&=t5_*80nbpkjm&=!'
+#os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = 1
+#int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")  
+ALLOWED_HOSTS = [
+    "www.giuliopierini.com",
+    "52.59.107.74", 
+    "localhost", 
+    "127.0.0.1", 
+    "[::1]",
+]
+#os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")  
 
 
 # Application definition
